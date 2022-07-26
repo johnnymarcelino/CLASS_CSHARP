657232486761fd9081a2506478e33851a2237357
Johnny Marcelino <johnnyg_engenheiro@hotmail.com>
2022-07-26 00:45:46 -0300

    Project of Class added

---
+++ Class/Program.cs
@@ -0,0 +1,19 @@
+﻿using Class.src;
+using System;
+
+namespace Class {
+    internal class Program {
+        static void Main(string[] args) {
+            Celular samsung = new Celular("Samsung", "S Galaxy 21+", 2021, 4800.00);
+            Celular apple = new Celular("Apple", "Iphone 13 Pro", 2021, 6500.00);
+            Celular motorolla = new Celular("Lenovo", "Moto Edge 30 Pro", 2022, 3999.00);
+            Celular xiomi = new Celular("Xiomi", "Mi 11 Ultra", 2021, 7890.00);
+
+            Console.WriteLine(samsung.ToString());
+            Console.WriteLine(apple.ToString());
+            Console.WriteLine(motorolla.ToString());
+            Console.WriteLine(xiomi.ToString());
+            Console.WriteLine(apple.Toque());
+        }
+    }
+}
---
+++ Class/src/Celular.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class.src {
+    public class Celular {
+
+        string Marca { get; set; }
+
+        string Modelo { get; set; }
+
+        int AnoLancamento { get; set; }
+
+        double Preco { get; set; }
+
+        public Celular(string marca, string modelo, int anoLancamento, double preco) {
+            Marca = marca;
+            Modelo = modelo;
+            AnoLancamento = anoLancamento;
+            Preco = preco;
+        }
+        public string Toque() {
+            return "O modelo Iphone é o única que tem um toque diferenciado: Trim, Trim";
+        }
+        public override string ToString() {
+            return ($"Este é um celular da marca {Marca} sendo do modelo {Modelo}, " +
+                $"com ano de lançamento em {AnoLancamento} e com um preço de {Preco} R$.");
+        }
+    }
+}
+
+//enum Categ {
+//    Botao,
+//    liga,
+//    desliga,
+//    tela
+//}
